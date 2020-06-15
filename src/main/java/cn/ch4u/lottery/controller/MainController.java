363adeb0ac33859bc10339a16c693e477c7ebab5
Michael-C-H <chenhao1993@126.com>
2020-06-15 23:36:27 +0800

    增加mp分页插件配置 增加开奖记录接口

--- src/main/java/cn/ch4u/lottery/controller/MainController.java
+++ src/main/java/cn/ch4u/lottery/controller/MainController.java
@@ -3,7 +3,11 @@ package cn.ch4u.lottery.controller;
 import cn.ch4u.lottery.constant.LotteryTypeEnum;
 import cn.ch4u.lottery.constant.RecommendEnum;
 import cn.ch4u.lottery.entity.HttpRes;
+import cn.ch4u.lottery.entity.Record;
 import cn.ch4u.lottery.service.IRecordService;
+import cn.ch4u.lottery.util.KwHelper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +39,19 @@ public class MainController {
         if (typeEnum==null || recommendEnum==null)
             return new HttpRes("参数错误！",null);
         return new HttpRes(recordService.recommend(typeEnum,recommendEnum));
+    }
+
+    @GetMapping("/records")
+    public HttpRes records(String typeKey,Integer pageIdx,Integer pageSize){
+        int page=1;
+        int size=10;
+        if (pageIdx!=null && pageIdx>0) page=pageIdx;
+        if (pageSize!=null) size=pageSize;
+
+        QueryWrapper<Record> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq(!KwHelper.isNullOrEmpty(typeKey),"typeKey",typeKey).orderByDesc("periodNo");
+        Page<Record> pageR=new Page<>(page,size,true);
+        Page<Record> plist=recordService.page(pageR,queryWrapper);
+        return new HttpRes(plist);
     }
 }
